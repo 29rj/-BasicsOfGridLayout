eaafae16b17c57448f4636531b77e61e80689495
Rishabh Raj <rishabhrr8253@gmail.com>
2021-05-12 20:59:43 +0530

    Rj:GridLayout plus some Sound Effect

---
+++ app/src/main/java/com/example/basicphrasesgrid/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.basicphrasesgrid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void playPhrases(View view)
+    {
+        Button pressed=(Button)view;
+
+        Log.i("Button pressed",pressed.getTag().toString());
+
+        MediaPlayer mediaPlayer=MediaPlayer.create(this,getResources().getIdentifier(pressed.getTag().toString(),"raw",getPackageName()));
+
+        mediaPlayer.start();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
